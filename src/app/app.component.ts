f7a2652e487e9c317a16a47c2583dc803efd7b0c
Harel <grinbergharelprojects@gmail.com>
2024-07-17 11:41:21 +0300

    connected to server with proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'temp';
+  sendServer() {
+    this._http.get<{ text: string }>('/api').subscribe((v) => {
+      console.log(v);
+    });
+  }
+  constructor(private readonly _http: HttpClient) {}
 }
